5b66b16320d1bfe668c9efe12ad090465a9b2cb8
MarkDavidBates <markbates.339@gmail.com>
2022-11-27 16:26:37 +0000

    added card and recycler view

--- app/src/main/java/org/wit/wishlistandroid/activities/WishlistListActivity.kt
+++ app/src/main/java/org/wit/wishlistandroid/activities/WishlistListActivity.kt
@@ -2,16 +2,56 @@ package org.wit.wishlistandroid.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.wit.wishlistandroid.R
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.wit.wishlistandroid.databinding.ActivityWishlistListBinding
+import org.wit.wishlistandroid.databinding.CardWishlistBinding
 import org.wit.wishlistandroid.main.MainApp
+import org.wit.wishlistandroid.models.WishlistModel
 
 class WishlistListActivity : AppCompatActivity() {
 
     lateinit var app: MainApp
+    private lateinit var binding: ActivityWishlistListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wishlist_list)
+        binding = ActivityWishlistListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = WishlistAdapter(app.wishlists)
+    }
+}
+
+class WishlistAdapter constructor(private var wishlists: List<WishlistModel>) :
+    RecyclerView.Adapter<WishlistAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardWishlistBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val wishlist = wishlists[holder.adapterPosition]
+        holder.bind(wishlist)
+    }
+
+    override fun getItemCount(): Int = wishlists.size
+
+    class MainHolder(private val binding : CardWishlistBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(wishlist: WishlistModel) {
+            binding.wishlistTitle.text = wishlist.title
+            binding.description.text = wishlist.description
+        }
     }
 }
--- app/src/main/java/org/wit/wishlistandroid/main/MainApp.kt
+++ app/src/main/java/org/wit/wishlistandroid/main/MainApp.kt
@@ -13,5 +13,6 @@ class MainApp: Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("Wishlist started")
+        wishlists.add(WishlistModel("bing", "chilling"))
     }
 }
